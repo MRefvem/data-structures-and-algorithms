eedf9c3338b1ebb0f76d1483352dae952b6c7489
Michael Refvem <mkrefvem@gmail.com>
2020-07-17 17:35:16 -0700

    Feature tasks complete, ACP then moving over to edit README in VS Code

---
+++ data-structures/StacksAndQueues/StacksAndQueues/Queue.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class Queue
+    {
+        private Node Front { get; set; }
+        private Node Rear { get; set; }
+
+        public Queue()
+        {
+            Rear = Front;
+        }
+
+        /// <summary>
+        /// Enqueue Method - takes in a string as an argument and adds a new node with that value to the front of the queue with an O(1) Time performance
+        /// </summary>
+        /// <param name="value">the string value you want to insert into the queue</param>
+        public void Enqueue(string value)
+        {
+            // Create a new node
+            Node node = new Node(value);
+
+            if (Front == null)
+            {
+                Front = node;
+                Rear = node;
+            }
+            else
+            {
+                Rear.Next = node;
+                Rear = node;
+            }
+        }
+
+        /// <summary>
+        /// Dequeue Method - removes the node from the front of the queue and returns the node's value. Raises and exception when called on an empty queue
+        /// </summary>
+        /// <returns>value of the node that was removed from the queue/returns>
+        public string Dequeue()
+        {
+            if (Front != null)
+            {
+                Node temp = Front;
+                Front = Front.Next;
+                temp.Next = null;
+                return temp.Value;
+            }
+            else
+            {
+                throw new Exception("Front is null");
+            }
+        }
+
+        /// <summary>
+        /// Peek Method - returns the value of the node located on front of the queue, without removing it from the queue
+        /// </summary>
+        /// <returns>the value of the node at the front of the queue if it exists</returns>
+        public string Peek()
+        {
+            if (Front != null)
+            {
+                return Front.Value;
+            }
+            else
+            {
+                throw new Exception("Front is null");
+            }
+        }
+
+        /// <summary>
+        /// IsEmpty Method - returns a boolean indicating whether or not the queue is empty
+        /// </summary>
+        /// <returns> a boolean</returns>
+        public bool IsEmpty()
+        {
+            if (Front == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ data-structures/StacksAndQueues/XUnitTestProject1/QueueTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using StacksAndQueues;
+using System.Linq.Expressions;
+
+namespace XUnitTestProject1
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void CanEnqueueIntoQueue()
+        {
+            // Arrange
+            Queue queue = new Queue();
+
+            // Act
+            queue.Enqueue("Josie Cat");
+            queue.Enqueue("Belle Kitty");
+            var peek = queue.Peek();
+
+            // Assert
+            Assert.Equal("Josie Cat", peek);
+            Assert.NotNull(peek);
+
+        }
+
+        [Fact]
+        public void CanEnqueueMultiple()
+        {
+            // Arrange
+            Queue queue = new Queue();
+
+            // Act
+            queue.Enqueue("Josie Cat");
+            queue.Enqueue("Belle Kitty");
+            queue.Enqueue("Mai Tai");
+            var peek = queue.Peek();
+            queue.Dequeue();
+            var peek2 = queue.Peek();
+            queue.Dequeue();
+            var peek3 = queue.Peek();
+
+            // Assert
+            Assert.Equal("Josie Cat", peek);
+            Assert.Equal("Belle Kitty", peek2);
+            Assert.Equal("Mai Tai", peek3);
+
+        }
+
+        [Fact]
+        public void CanSuccessfullyDequeue()
+        {
+            // Arrange
+            Queue queue = new Queue();
+
+            // Act
+            queue.Enqueue("Josie Cat");
+            queue.Enqueue("Belle Kitty");
+            queue.Enqueue("Mai Tai");
+            queue.Enqueue("Misu");
+            queue.Enqueue("Tony");
+            var dequeue = queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            var dequeue2 = queue.Dequeue();
+
+            // Assert
+            Assert.Equal("Josie Cat", dequeue);
+            Assert.Equal("Tony", dequeue2);
+
+        }
+
+        [Fact]
+        public void CanPeekIntoQueue()
+        {
+            // Arrange
+            Queue queue = new Queue();
+
+            // Act
+            queue.Enqueue("Josie Cat");
+            queue.Enqueue("Belle Kitty");
+            queue.Enqueue("Mai Tai");
+            queue.Enqueue("Misu");
+            queue.Enqueue("Tony");
+            var peek = queue.Peek();
+
+            // Assert
+            Assert.Equal("Josie Cat", peek);
+
+        }
+
+        [Fact]
+        public void CanSuccessfullyEmpty()
+        {
+            // Arrange
+            Queue queue = new Queue();
+
+            // Act
+            queue.Enqueue("Josie Cat");
+            queue.Enqueue("Belle Kitty");
+            queue.Enqueue("Mai Tai");
+            queue.Enqueue("Misu");
+            queue.Enqueue("Tony");
+            var dequeue = queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            var peek = queue.Peek();
+            var dequeue2 = queue.Dequeue();
+            Exception e = Assert.Throws<Exception>(() => queue.Peek());
+
+            // Assert
+            Assert.Equal("Josie Cat", dequeue);
+            Assert.Equal("Tony", dequeue2);
+            Assert.NotNull(peek);
+            Assert.Equal("Front is null", e.Message);
+
+        }
+
+        [Fact]
+        public void CanInstantiateEmpty()
+        {
+            // Arrange
+            Queue queue = new Queue();
+
+            // Act & Assert
+            Assert.True(queue.IsEmpty());
+
+        }
+
+        [Fact]
+        public void CallingOnEmptyRaisesException()
+        {
+            // Arrange
+            Queue queue = new Queue();
+
+            // Act
+            Exception e = Assert.Throws<Exception>(() => queue.Dequeue());
+            Exception f = Assert.Throws<Exception>(() => queue.Peek());
+
+            // Assert
+            Assert.Equal("Front is null", e.Message);
+            Assert.Equal("Front is null", f.Message);
+
+        }
+    }
+}
