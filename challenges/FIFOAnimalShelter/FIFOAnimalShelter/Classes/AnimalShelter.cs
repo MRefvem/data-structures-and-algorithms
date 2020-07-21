f03796768be366d9a85bf6cf522d4ee800610696
Michael Refvem <mkrefvem@gmail.com>
2020-07-21 16:51:02 -0700

    Code challenge 12 started

---
+++ challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    class AnimalShelter
+    {
+        public int MyProperty { get; set; }
+    }
+}
